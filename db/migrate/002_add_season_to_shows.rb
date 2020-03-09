cb998acbd5a6bbfa75632f0fc9960c15e897a5f9
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-03-09 04:08:54 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,7 +1,5 @@
-class AddSeasonToShows < ActiveRecord::Migration[5.2]
-  
+class AddSeasonToShows < ActiveRecord::Migration
   def change
     add_column :shows, :season, :string
   end
-  
-end  
+end
