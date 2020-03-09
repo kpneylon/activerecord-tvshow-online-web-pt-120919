22f272d1acfc31b819b584f92102efa2d04a68be
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-03-09 03:32:35 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,11 +1,10 @@
 class CreateShow < ActiveRecord::Migrate[5.2]
   def change
     create_table :shows do |t|
-      t.string :title
-      t.integer :release_date
-      t.string :director
-      t.string :lead
-      t.boolean :in_theaters
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
     end
   end
 end  
