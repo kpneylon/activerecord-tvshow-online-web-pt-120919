3d4d62cda281ea2def8f4ad20cf4d10e16467ef8
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-03-09 03:29:57 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,3 +1,11 @@
 class CreateShow < ActiveRecord::Migrate
-  
+  def change
+    create_table :shows do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+    end
+  end
 end  
